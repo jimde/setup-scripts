48f217d1c94deb5c1ff00ced9470c390c3eb8040
jimmy <livingoscilloscope@gmail.com>
2018-02-16 15:08:06 -0500

    updated scripts

---
+++ reduce-titlebar-size.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+##############################
+# reduce size of titlebars
+##############################
+cat setup-jimmy-ubuntu-gnome-titlebars.css >> ~/.config/gtk-3.0/gtk.css
+# press alt + F2, then type 'r' to reload gnome shell
+# https://unix.stackexchange.com/questions/276951/how-to-change-the-titlebar-height-in-standard-gtk-apps-and-those-with-headerbars
