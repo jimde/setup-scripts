df85aafc59a3ac8719e213fd203f42358cae64cd
Jimmy Deng <jimde@users.noreply.github.com>
2018-06-07 23:05:56 -0400

    updated realsense installation script

--- install-realsense.sh
+++ install-realsense.sh
@@ -3,12 +3,15 @@
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install libusb-1.0-0-dev pkg-config
 sudo apt-get install libglfw3-dev
+sudo apt-get install build-essential libgtk-3-dev
+sudo apt-get install cmake
 
 git clone https://github.com/IntelRealSense/librealsense.git
 cd librealsense
+git checkout tags/v1.12.1
 mkdir build
 cd build
 
 cmake .. -DBUILD_EXAMPLES:BOOL=true
-make && sudo make install
+make -j4 && sudo make install
 sudo ldconfig
