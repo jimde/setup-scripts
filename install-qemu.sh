1b37d2693ef5b47cc5bce071f71d86459b8786de
jimmy <livingoscilloscope@gmail.com>
2018-12-01 22:06:34 -0500

    added qemu and latex scripts

--- install-qemu.sh
+++ install-qemu.sh
@@ -5,3 +5,6 @@ sudo apt install -y build-essential
 sudo apt install -y pkg-config
 sudo apt install -y libglib2.0-dev
 sudo apt install -y libpixman-1-dev
+
+# sudo ln -s /home/jimmy/Documents/install-qemu/qemu-3.1.0-rc3/aarch64-softmmu/qemu-system-aarch64 qemu-system-aarch64
+
