d78762f221f475d5c13d94ea5789631b5184e581
Jimmy Deng <jimde@users.noreply.github.com>
2020-03-09 16:26:21 -0400

    update photography robocopy script

--- windows-scripts/photography-network-backup.bat
+++ windows-scripts/photography-network-backup.bat
@@ -1,4 +1,4 @@
-set DESTINATION="X:\Photography"
+set DESTINATION=X:\Photography
 set RC_FLAGS=/S /E /tee /R:10 /W:10 /MT:8
 
 for /f "tokens=1-3 delims=/ " %%a in ('date /t') do (set _date=%%a%%b%%c)
