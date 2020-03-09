8def898939c7f4d9b5045b08d1ed9c64557e687f
Jimmy Deng <jimde@users.noreply.github.com>
2020-03-09 16:26:55 -0400

    update general robocopy script

--- windows-scripts/network-backup.bat
+++ windows-scripts/network-backup.bat
@@ -1,4 +1,4 @@
-set DESTINATION_ROOT="X:\RAZER-BACKUP"
+set DESTINATION_ROOT=X:\RAZER-BACKUP
 set RC_FLAGS=/S /E /tee /R:10 /W:10 /MT:8
 
 for /f "tokens=1-3 delims=/ " %%a in ('date /t') do (set _date=%%a%%b%%c)
