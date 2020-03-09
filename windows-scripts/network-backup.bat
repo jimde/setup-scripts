5e5b549c0e300487bb6b8bd9e6f03c0cdd8a9ba7
Jimmy Deng <jimde@users.noreply.github.com>
2020-03-09 16:40:20 -0400

    update general robocopy script

--- windows-scripts/network-backup.bat
+++ windows-scripts/network-backup.bat
@@ -1,11 +1,11 @@
 set DESTINATION_ROOT=X:\RAZER-BACKUP
-set RC_FLAGS=/S /E /tee /R:10 /W:10 /MT:8
+set RC_FLAGS=/S /E /XX /TEE /R:10 /W:10 /MT:16
 
 for /f "tokens=1-3 delims=/ " %%a in ('date /t') do (set _date=%%a%%b%%c)
 for /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set _time=%%a%%b)
 
-robocopy %RC_FLAGS% /log+:"LOGS\network-backup-%_date%_%_time%-Pictures.log" "C:\Users\jimmy\Pictures" "%DESTINATION_ROOT%\Pictures"
-robocopy %RC_FLAGS% /log+:"LOGS\network-backup-%_date%_%_time%-Videos.log" "C:\Users\jimmy\Videos" "%DESTINATION_ROOT%\Videos"
-robocopy %RC_FLAGS% /log+:"LOGS\network-backup-%_date%_%_time%-Documents.log" "C:\Users\jimmy\Documents" "%DESTINATION_ROOT%\Documents"
+robocopy "C:\Users\jimmy\Pictures" "%DESTINATION_ROOT%\Pictures" %RC_FLAGS% /log+:"LOGS\network-backup-%_date%_%_time%-Pictures.log" /XD C:\Users\jimmy\Pictures\Photography
+robocopy "C:\Users\jimmy\Videos" "%DESTINATION_ROOT%\Videos" %RC_FLAGS% /log+:"LOGS\network-backup-%_date%_%_time%-Videos.log"
+robocopy "C:\Users\jimmy\Documents" "%DESTINATION_ROOT%\Documents" %RC_FLAGS% /log+:"LOGS\network-backup-%_date%_%_time%-Documents.log"
 
 pause
