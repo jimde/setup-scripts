100e725af932c3ba4e40c5cd7f526ee29c6c4963
jimmy <livingoscilloscope@gmail.com>
2020-03-12 22:18:45 -0400

    update backup scripts

--- backup-scripts/razer/downloads-network-backup.bat
+++ backup-scripts/razer/downloads-network-backup.bat
@@ -4,6 +4,8 @@ set RC_FLAGS=/S /E /XX /TEE /R:10 /W:10 /MT:16
 for /f "tokens=1-3 delims=/ " %%a in ('date /t') do (set _date=%%a%%b%%c)
 for /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set _time=%%a%%b)
 
+mkdir LOGS\razer
+
 robocopy "C:\Users\jimmy\Downloads" "%DESTINATION_ROOT%\Downloads" %RC_FLAGS% /log+:"LOGS\razer\network-backup-%_date%_%_time%-Downloads.log"
 
 pause
--- backup-scripts/razer/network-backup.bat
+++ backup-scripts/razer/network-backup.bat
@@ -4,6 +4,8 @@ set RC_FLAGS=/S /E /XX /TEE /R:10 /W:10 /MT:16
 for /f "tokens=1-3 delims=/ " %%a in ('date /t') do (set _date=%%a%%b%%c)
 for /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set _time=%%a%%b)
 
+mkdir LOGS\razer
+
 robocopy "C:\Users\jimmy\Pictures" "%DESTINATION_ROOT%\Pictures" %RC_FLAGS% /log+:"LOGS\razer\network-backup-%_date%_%_time%-Pictures.log" /XD C:\Users\jimmy\Pictures\Photography
 robocopy "C:\Users\jimmy\Videos" "%DESTINATION_ROOT%\Videos" %RC_FLAGS% /log+:"LOGS\razer\network-backup-%_date%_%_time%-Videos.log"
 robocopy "C:\Users\jimmy\Documents" "%DESTINATION_ROOT%\Documents" %RC_FLAGS% /log+:"LOGS\razer\network-backup-%_date%_%_time%-Documents.log" /XD "C:\Users\jimmy\Documents\Projects"
